c9a36c08a2b8ac476cbae1b1883b69909e455aff
devSaifur <saifur2akash@gmail.com>
2024-04-19 19:56:54 +0600

    basic ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,34 @@
-import Link from "next/link";
+import Image from 'next/image'
+
+const mokeUrl = [
+  'https://utfs.io/f/3f9a09cd-7c85-42de-b591-7a029514b57a-tqema4.jpg',
+  'https://utfs.io/f/13ff4288-4a2e-4e92-a630-ed5af2cdcfea-41owxh.webp',
+  'https://utfs.io/f/bd98fb8e-ad8c-46dd-b2f7-99629a1b3c03-i4adbz.webp',
+  'https://utfs.io/f/d63e6dfb-8b52-4efa-a620-04f2917dc467-msven3.jpg',
+]
+
+const mokeImages = mokeUrl.map((url, i) => {
+  return {
+    id: i + 1,
+    src: url,
+  }
+})
 
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-        <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-          Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
-        </h1>
-        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-            href="https://create.t3.gg/en/usage/first-steps"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">First Steps →</h3>
-            <div className="text-lg">
-              Just the basics - Everything you need to know to set up your
-              database and authentication.
-            </div>
-          </Link>
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-            href="https://create.t3.gg/en/introduction"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">Documentation →</h3>
-            <div className="text-lg">
-              Learn more about Create T3 App, the libraries it uses, and how to
-              deploy it.
-            </div>
-          </Link>
-        </div>
+    <main className="mx-auto max-w-[90rem]">
+      <div className="flex flex-wrap gap-4">
+        {[...mokeImages, ...mokeImages, ...mokeImages].map((image) => (
+          <div className="size-56 relative mx-auto" key={image.id}>
+            <Image
+              src={image.src}
+              sizes="(max-width: 768px) 20vw, 30vw"
+              fill
+              alt=""
+            />
+          </div>
+        ))}
       </div>
     </main>
-  );
+  )
 }
